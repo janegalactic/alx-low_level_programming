2b864db61edb5abb5ebe5007f980b33764cac0c9
janegalactic <jwirungu1@gmail.com>
2022-12-15 01:36:07 -0800

    except 2 and 4

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_numbers - print numbers
- *
+ *@c: char to check
  * Return: void
  */
 
@@ -12,7 +12,7 @@ void print_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar (c);
+		_putchar(c);
 	}
 	_putchar ('\n');
 }
