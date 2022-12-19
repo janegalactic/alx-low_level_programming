2ca4e4c5236e6601ac015fa8392393144473abeb
janegalactic <jwirungu1@gmail.com>
2022-12-19 02:13:57 -0800

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints reverse
+ * @s: char to check
+ *
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int b, n;
+
+	n = 0;
+
+	while (s[n] != '\0')
+		n++;
+	for (b = n - 1; b >= 0; b--)
+	{
+		_putchar(s[b]);
+	}
+	_putchar('\n')
+}
