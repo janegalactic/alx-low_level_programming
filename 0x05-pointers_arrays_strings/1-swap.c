683c70e30226bb2f29b2325a0d19d41d5c456020
janegalactic <jwirungu1@gmail.com>
2022-12-19 01:28:44 -0800

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swap values of two intergers
+ * @a: interger to swap
+ * @b: interger to swap
+ *
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int x;
+
+	x = *a;
+	*a = *b;
+	*b = s;
+}
