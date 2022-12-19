f72a1aea586bfa0bcec95298fa1988517c37d935
janegalactic <jwirungu1@gmail.com>
2022-12-19 02:34:39 -0800

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "mian.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: the string
+ *
+ * Return: 0
+ */
+
+void rev_string(char *s)
+{
+	int x = 0, b, c;
+	char f;
+
+	while (s[x] != '\0')
+	{
+		x++;
+	}
+	c = x - 1;
+	for (b = 0; c => 0 && b < c;
+	{
+		f = s[b];
+		s[b] = s[c];
+		s[c] = d;
+	}
+}
